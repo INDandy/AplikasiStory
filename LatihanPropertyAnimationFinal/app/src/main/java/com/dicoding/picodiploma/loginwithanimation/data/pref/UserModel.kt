22537775ec6063b259788a4fc4dcb15e22f6a9a7
dandy <dandy.royyan.firdaus@gmail.com>
2024-11-29 03:04:56 +0700

    Done!

--- LatihanPropertyAnimationFinal/app/src/main/java/com/dicoding/picodiploma/loginwithanimation/data/pref/UserModel.kt
+++
@@ -1,8 +0,0 @@
-package com.dicoding.picodiploma.loginwithanimation.data.pref
-
-data class UserModel(
-    val email: String,
-    val password: String,
-    val token: String,
-    val isLogin: Boolean = false
-)
